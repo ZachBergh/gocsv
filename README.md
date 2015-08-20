9efff44cd7e18e7a5888b07953b1b1744ad4b9bd
Zach Bergh <berghzach@gmail.com>
2015-08-20 18:24:21 +0800

    test readme

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
+### Gocsv
+
+A data to csv file productor tool.
+
+##### Installation
+````
+go get github.com/ZachBergh/gocsv
+````
+
+##### Usage
+
 ````
 import "github.com/ZachBergh/gocsv"
 
-...
-// fileName, path string
-// header []string, if len(header) <= 0 , will create default header
-// data []interface{}
+/* 
+Params:
+    fileName string: csv file's name 
+    path string: csv file's path
+    header []string: csv file's header
+        if len(header) <= 0 , will create default header
+    data []interface{}: source data
+*/
+
 err := gocsv.NewCsvFile(fileName, path, header, data)
+if err != nil {
+    return err
+}
 ````
---
+++ gocsv_test.go
@@ -0,0 +1,34 @@
+package gocsv
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+func Test_Gocsv(t *testing.T) {
+
+	resp, err := http.Get("https://api.github.com/search/code?q=addClass+in:file+language:js+repo:jquery/jquery")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	defer resp.Body.Close()
+	request, err := ioutil.ReadAll(resp.Body)
+
+	var data struct {
+		TotalCount       int           `json:"total_count"`
+		IncompleteResult bool          `json:"incomplete_results"`
+		Items            []interface{} `json:"items"`
+	}
+	err = json.Unmarshal(request, &data)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	err = NewCsvFile("test.csv", "", []string{}, data.Items)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	t.Log("success")
+}
